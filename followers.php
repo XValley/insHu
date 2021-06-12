2678d6cf27134fe44e1db7adaa58455d3931a5f3
XValley <85756209+XValley@users.noreply.github.com>
2021-06-12 13:28:29 +0300

    Update followers.php

--- followers.php
+++ followers.php
@@ -8,25 +8,25 @@ $id = $config['words'];
 $file = $config['for'];
 $a = file_exists('a') ? file_get_contents('a') : 'ap';
 if($a == 'new'){
- file_put_contents($file, '');
+	file_put_contents($file, '');
 }
 $from = 'Followers';
 $mid = bot('sendMessage',[
- 'chat_id'=>$config['id'],
- 'message_id'=>$mid,
- 'text'=>"*Collection From* ~ [ _ $from _ ]\n\n*Status* ~> _ Working _\n*Users* ~> _ ".count(explode("\n", file_get_contents($file)))."_",
- 'parse_mode'=>'markdown',
- 'reply_markup'=>json_encode(['inline_keyboard'=>[
-   [['text'=>'Stop.','callback_data'=>'stopgr']]
-  ]])
+	'chat_id'=>$config['id'],
+	'message_id'=>$mid,
+	'text'=>"*Collection From* ~ [ _ $from _ ]\n\n*Status* ~> _ Working _\n*Users* ~> _ ".count(explode("\n", file_get_contents($file)))."_",
+	'parse_mode'=>'markdown',
+	'reply_markup'=>json_encode(['inline_keyboard'=>[
+			[['text'=>'Stop.','callback_data'=>'stopgr']]
+		]])
 ])->result->message_id;
 $ids = explode(' ', $config['words']);
 foreach($ids as $user){
- echo $user."\n";
- sleep(5);
- $cookies = $accounts[$file];
- $ig = new ig(['account'=>$accounts[$file],'file'=>$file]);
- $info = $ig->getInfo($user);
- $id = $info->pk;
- $ig->getFollowers($id,$mid,$user);
+	echo $user."\n";
+	sleep(5);
+	$cookies = $accounts[$file];
+	$ig = new ig(['account'=>$accounts[$file],'file'=>$file]);
+	$info = $ig->getInfo($user);
+	$id = $info->pk;
+	$ig->getFollowers($id,$mid,$user);
 }
